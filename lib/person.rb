aebeb42de26ddf3efb26a7b53f426dcdeefc004f
Christopher Parsons <mail@christopherparsons.me>
2018-05-15 01:17:19 +0100

    full_name refactor Found that with this example an array was much easier to manipulate than individual strings. As such, created an array of the arguments and joined them. Used .compact to remove nil values, reducing the need for an if.

--- lib/person.rb
+++ lib/person.rb
@@ -5,12 +5,10 @@ class Person
     @last_name = last_name
   end
 
-  def full_name (first_name, middle_name, last_name)
-    if middle_name == nil
-      first_name + " " + last_name
-    else
-      first_name + " " + middle_name + " " + last_name
-    end
+  def full_name (*names)
+    names.compact.join(" ")
+    # .compact removes any nil values from an array.
+    # This resolves the extra spaces issue without ifs.
   end
 
   def full_name_with_middle_initial(first_name, middle_name, last_name)
